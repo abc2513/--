69498848d9c030704d6ba1de4ad50c29c57409db
orange-q <2258774026@qq.com>
2023-11-08 19:05:23 +0800

    publish_only

--- 实验1/实验代码/main.js
+++ 实验1/实验代码/main.js
@@ -1,18 +1,18 @@
 class PubSub {//发布订阅模式，事件的调度中心
-    constructor() { 
+    constructor() {
         this.message = {
             //"频道key":[订阅的模块处理函数， ...]
         };
     };
-    
+
     publish(type, data) {
-        console.log('调度中心发布事件',type,data);
-        if(type===null){
+        console.log('调度中心发布事件', type, data);
+        if (type === null) {
             //广播模式
             for (const key in this.message) {
                 this.message[key].forEach(item => item(data))
             }
-            return;
+            return
         }
         if (this.message[type]) {
             this.message[type].forEach(item => item(data))
@@ -24,7 +24,7 @@ class PubSub {//发布订阅模式，事件的调度中心
             this.message[type].push(cb);
         }
         else {
-            this.message[type]=[cb];
+            this.message[type] = [cb];
         }
     }
     unSubscribe(type, cb) {//取消订阅
@@ -34,7 +34,7 @@ class PubSub {//发布订阅模式，事件的调度中心
     }
 }
 class publisher {//发布者
-    constructor(pubSub, types ) { //传入调度中心和实例
+    constructor(pubSub, types) { //传入调度中心和实例
         this.pubSub = pubSub;
         this.types = types;
 
@@ -42,27 +42,27 @@ class publisher {//发布者
         //创建内嵌标题和input的div
         this.div = document.createElement("div");
         this.h3 = document.createElement("div");
-        this.h3.innerText=`发布者（频道${this.types.map(item=>item??'all'+'；')}）：`;
+        this.h3.innerText = `发布者（频道${this.types.map(item => item ?? 'all' + '；')}）：`;
         this.input = document.createElement("input");
         this.div.appendChild(this.h3);
         this.div.appendChild(this.input);
-        this.div.style.display="flex";
+        this.div.style.display = "flex";
         document.body.appendChild(this.div);
         //绑定失焦事件
-        this.input.addEventListener("blur",()=>{
+        this.input.addEventListener("blur", () => {
             console.log('输入框失焦');
             this.publish(this.input.value);
         })
     };
     publish(data) {
-        console.log('发布者发布事件',this.types,data);
+        console.log('发布者发布事件', this.types, data);
         this.types.forEach(type => {
             this.pubSub.publish(type, data);
         });
     }
 }
 class subscriber {//订阅者
-    constructor(pubSub,types) {
+    constructor(pubSub, types) {
         this.pubSub = pubSub;
         this.types = types;
         this.types.forEach(type => {
@@ -72,25 +72,29 @@ class subscriber {//订阅者
         //以下为数据可视化部分
         //新建一个div
         this.div = document.createElement("div");
-        this.div.innerText = `订阅者（频道${this.types.map(item=>item+'；')}）：`;
+        this.div.innerText = `订阅者（频道${this.types.map(item => item + '；')}）：`;
         document.body.appendChild(this.div);
     }
-    cb=(data)=>{//接受到消息的处理函数
-        console.log('订阅者接受到事件',data);
-        this.div.innerText+=data+' ；';
+    cb = (data) => {//接受到消息的处理函数
+        console.log('订阅者接受到事件', data);
+        this.div.innerText += data + ' ；';
     }
 }
 const pubSub = new PubSub();
 const publisher_a = new publisher(pubSub, ['a']);//创建一个发布者，发布频道为a
 const publisher_b = new publisher(pubSub, ['b']);//创建一个发布者，发布频道为b
-const publisher_ab = new publisher(pubSub, ['a','b']);//创建一个发布者，发布频道为a和b
-const publisher_all= new publisher(pubSub, [null]);//创建一个发布者，为广播模式
-const subscriber_a = new subscriber(pubSub,['a']);//创建一个订阅者，订阅频道为a
-const subscriber_b = new subscriber(pubSub,['b']);//创建一个订阅者，订阅频道为b
-const subscriber_ab = new subscriber(pubSub,['a','b']);//创建一个订阅者，订阅频道为a和b
-const subscribe_c= new subscriber(pubSub,['c']);//创建一个订阅者，订阅频道为c
+const publisher_ab = new publisher(pubSub, ['a', 'b']);//创建一个发布者，发布频道为a和b
+const publisher_all = new publisher(pubSub, [null]);//创建一个发布者，为广播模式
+const subscriber_a = new subscriber(pubSub, ['a']);//创建一个订阅者，订阅频道为a
+const subscriber_b = new subscriber(pubSub, ['b']);//创建一个订阅者，订阅频道为b
+const subscriber_ab = new subscriber(pubSub, ['a', 'b']);//创建一个订阅者，订阅频道为a和b
+const subscribe_c = new subscriber(pubSub, ['c']);//创建一个订阅者，订阅频道为c
 
 
-const publisher_only= new publisher(pubSub, []);//创建一个发布者，发布频道为空
+const publisher_only = new publisher(pubSub, []);//创建一个发布者，发布频道为空
+const subscribe_only = new subscriber(pubSub, [])
 //点对点模式
-//TODO
+publisher_only.publish = function (data) {
+    console.log("发布者发布信息：", data);
+    subscribe_only.cb(data)
+}
